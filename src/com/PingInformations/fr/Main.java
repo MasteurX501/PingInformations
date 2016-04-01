541c2b2e9b3b58b1eb4208c86e2735bb11b26a3d
Ben <benjibenji501@yahoo.fr>
2016-04-02 00:31:58 +0200

    Update #1 Default
    Ajoute des packets pas défaut

---
+++ src/com/PingInformations/fr/Main.java
@@ -0,0 +1,15 @@
+package com.PingInformations.fr;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisabled(){
+		
+	}
+
+}
